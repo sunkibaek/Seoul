f2e3d975950f46ba263f6ed62688732d5b85ccb9
Sunki Baek <sunki.baek@gmail.com>
2018-10-30 14:39:19 -0700

    Apply prettier

--- src/my-command.js
+++ src/my-command.js
@@ -1,23 +1,27 @@
-const sketch = require('sketch')
-const { DataSupplier } = sketch
-const util = require('util')
+const sketch = require("sketch");
+const { DataSupplier } = sketch;
+const util = require("util");
 
-export function onStartup () {
-  // To register the plugin, uncomment the relevant type:
-  DataSupplier.registerDataSupplier('public.text', 'Seoul', 'SupplyData')
-  // DataSupplier.registerDataSupplier('public.image', 'Seoul', 'SupplyData')
+export function onStartup() {
+  DataSupplier.registerDataSupplier(
+    "public.text",
+    "Cities in Korea",
+    "SupplyData"
+  );
 }
 
-export function onShutdown () {
-  // Deregister the plugin
-  DataSupplier.deregisterDataSuppliers()
+export function onShutdown() {
+  DataSupplier.deregisterDataSuppliers();
 }
 
-export function onSupplyData (context) {
-  let dataKey = context.data.key
-  const items = util.toArray(context.data.items).map(sketch.fromNative)
+export function onSupplyData(context) {
+  let dataKey = context.data.key;
+
+  const items = util.toArray(context.data.items).map(sketch.fromNative);
+
   items.forEach((item, index) => {
-    let data = Math.random().toString()
-    DataSupplier.supplyDataAtIndex(dataKey, data, index)
-  })
+    let data = Math.random().toString();
+
+    DataSupplier.supplyDataAtIndex(dataKey, data, index);
+  });
 }
